1dca4761e5b57ed28e609a24b3df0ec2c4312d9a
NateDaveHill <69320372+NateDaveHill@users.noreply.github.com>
2025-11-24 16:51:20 +0100

    load Env func

--- Controller/config.php
+++ Controller/config.php
@@ -1,3 +1,24 @@
 <?php
 
+function loadEnv($path) {
+    if (!file_exists($path)) {
+        return false;
+    }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $lines) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);\
+        $value = trim($value);
+
+        if(!array_key_exists($name, $_ENV)) {
+            $_ENV[$name] = $value;
+        }
+    }
+    return true;
+}
 
