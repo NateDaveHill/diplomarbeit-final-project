c0305c64e4934629ae0088a43e39033f72030e62
NateDaveHill <69320372+NateDaveHill@users.noreply.github.com>
2025-12-05 15:29:04 +0100

    fetching single prod from db

--- View/product.php
+++ View/product.php
@@ -1,1 +1,14 @@
 <?php
+require_once __DIR__ . '/../Controller/config.php';
+require_once __DIR__ . '/../Controller/cart_handler.php';
+
+$product_id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+$stmt = $pdo->prepare("SELECT * FROM products WHERE id = ?");
+$stmt->execute([$product_id]);
+$product = $stmt->fetch();
+
+if (!$product) {
+    redirect('index.php');
+}
+?>
